ba6b3e52d89aeffaea3c680528c833993c91d3bd
abdalrhman <abdalrhmanmogahed2002@gmail.com>
2026-05-25 23:58:23 +0300

    Implement authentication endpoints, start doing exception handling

--- src/main/java/com/mujahid/trackify/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mujahid.trackify.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
